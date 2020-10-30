8cb09a984e23dd6a28a9f0380eedd122c9f23b37
SDudyuk <s.dudyuk.o@gmail.com>
2020-10-30 18:05:15 +0200

    get organ_name

--- load3.js
+++ load3.js
@@ -3,12 +3,17 @@ const xml2js = require('xml2js');
 const parser = new xml2js.Parser();
 var iconv = require('iconv-lite');
 
-const path = "test.xml";
-
 var buff = fs.readFileSync('test.xml');
 var buffer = iconv.decode(buff, 'win1251');
 
 parser.parseString(buffer, function(err, res) {
-  var txt = res.DATA.RECORD[0].SURNAME[0];
+  for (const item of res.DATA.RECORD) {
+    var txt = item.ORG_NAME[0] + " - " + item.REGION_NAME[0];
+    console.log(txt);
+  }
+/*
+    console.log(res.DATA.RECORD.length);
+  var txt = res.DATA.RECORD[0].ORG_NAME[0] + " - " + res.DATA.RECORD[0].REGION_NAME[0];
   console.log(txt);
+*/  
 });
