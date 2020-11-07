b764928b72cc6c2964ace8f82b2b9505bb1d6324
SDudyuk <s.dudyuk.o@gmail.com>
2020-11-07 12:58:30 +0200

    get organ_name from xml

--- load3.js
+++ load3.js
@@ -3,13 +3,15 @@ const xml2js = require('xml2js');
 const parser = new xml2js.Parser();
 var iconv = require('iconv-lite');
 
-var buff = fs.readFileSync('test.xml');
+//var buff = fs.readFileSync('test.xml');
+var buff = fs.readFileSync('19-ex_xml_EXPERT.xml');
 var buffer = iconv.decode(buff, 'win1251');
 
 parser.parseString(buffer, function(err, res) {
   for (const item of res.DATA.RECORD) {
     var txt = item.ORG_NAME[0] + " - " + item.REGION_NAME[0];
-    console.log(txt);
+    //console.log(txt);
+    fs.appendFileSync('organ.txt', txt+"\r\n");
   }
 /*
     console.log(res.DATA.RECORD.length);
